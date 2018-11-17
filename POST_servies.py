4cb6cbc3dc1555a3078cbae950ee00f604c32845
Viacheslav <mrSlavik92@gmail.com>
2018-11-17 20:23:30 +0200

    Add send to different ports

--- POST_servies.py
+++ POST_servies.py
@@ -10,12 +10,18 @@ def posting():
     Team = data[1]
     Employee = data[2]
 
-    def send_json(sendjson):
-        sendjson = post("https://reqres.in/api/users", json=sendjson, headers={"Content-type": "application/json"})
-        print(sendjson.text)
-    send_json(Department)
-    send_json(Team)
-    send_json(Employee)
+    def send_json1(sendjson1):
+        sendjson1 = post("https://reqres.in/api/users:5001", json=sendjson1, headers={"Content-type": "application/json"})
+        print(sendjson1.text)
+    def send_json2(sendjson2):
+        sendjson2 = post("https://reqres.in/api/users:5002", json=sendjson2, headers={"Content-type": "application/json"})
+        print(sendjson2.text)
+    def send_json3(sendjson3):
+        sendjson3 = post("https://reqres.in/api/users:5003", json=sendjson3, headers={"Content-type": "application/json"})
+        print(sendjson3.text)
+    send_json1(Department)
+    send_json2(Team)
+    send_json3(Employee)
 
     return jsonify(Department, Team, Employee)
 
