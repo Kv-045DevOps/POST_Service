b4a662317d2013889c5d0bfd852127bded2d9122
Viacheslav <mrSlavik92@gmail.com>
2018-12-21 00:00:49 +0200

    Post_service_v2
    Add check null in json, add return to UI, add oc.getenv in #

--- POST_servies.py
+++ POST_servies.py
@@ -1,24 +1,49 @@
 from flask import Flask, jsonify, request
 from requests import post
 import json
+#import os
 
 app = Flask(__name__)
+# link = str(os.getenv("DB_URL"))
+link = "reqres.in/api/users"
+
 
 @app.route('/posting', methods=['POST'])
 def posting():
-
     def send_json1(sendjson1):
-        sendjson1 = post("https://127.0.0.1:5002/depart", json=sendjson1, headers={"Content-type": "application/json"})
+        # department send function
+        sendjson1 = post("https://" + link + ":5001", json=sendjson1, headers={"Content-type": "application/json"})
         print(sendjson1.text)
+
     def send_json2(sendjson2):
-        sendjson2 = post("https://127.0.0.1:5002/team", json=sendjson2, headers={"Content-type": "application/json"})
+        # Team send function
+        sendjson2 = post("https://" + link + ":5002", json=sendjson2, headers={"Content-type": "application/json"})
         print(sendjson2.text)
+
     def send_json3(sendjson3):
-        sendjson3 = post("https://127.0.0.1:5002/users", json=sendjson3, headers={"Content-type": "application/json"})
+        # Employee send function
+        sendjson3 = post("https://" + link + ":5003", json=sendjson3, headers={"Content-type": "application/json"})
         print(sendjson3.text)
+
+    def json_count(little_json):
+        # Check null in json function
+        len_keys = len(little_json)
+        len_value = 0
+        for i in little_json:
+            n = little_json[i]
+            if n == None:
+                len_value = 0
+            else:
+                len_value += 1
+
+        if len_value == len_keys:
+            return True
+        else:
+            return False
 
     datajson = request.data
     data = json.loads(datajson)
+    msg = "Successfully sent"
 
     for i in data:
         if i == "department":
@@ -26,35 +51,35 @@ def posting():
             if Department == None:
                 pass
             else:
-                send_json1(Department)
+                if json_count(Department) == True:
+                    send_json1(Department)
+                else:
+                    msg = "Fill in all the fields"
+
         elif i == "team":
             Team = data["team"]
             if Team == None:
                 pass
             else:
-                send_json2(Team)
+                if json_count(Team) == True:
+                    send_json2(Team)
+                else:
+                    msg = "Fill in all the fields"
+
         elif i == "employee":
             Employee = data["employee"]
             if Employee == None:
                 pass
             else:
-                send_json3(Employee)
+                if json_count(Employee) == True:
+                    send_json3(Employee)
+                else:
+                    msg = "Fill in all the fields"
         else:
             pass
+    return jsonify(msg)
 
-    """{"department": {"name": "1"}, "team": {"depart_id": "1", "name": "1", "manager_id": "1"}, "employee": {"team_id": "1", "name": "1", "sname": "1", "exp": "1", "position": "1", "salary": "1", "coefficient": "1"}}"""
-
-
-    """ version for another format json:
-    # data = request.get_json()
-    # Department = data[0]
-    # Team = data[1]
-    # Employee = data[2]
-    
-    [{"department":{"name": "Ben"}},{"team":{"depart_id":"1","name":"Viktor","manager_id":"5"}},{"employee":{"team_id":"11","name":"Dmitriy","sname":"Dimin","exp":"3","position":"Manager","salary":"1500","coefficient":"1"}}]"""
-
-    return ""
-    #jsonify(data)
+    # Example json: {"department": {"name": "1"}, "team": {"depart_id": "1", "name": "1", "manager_id": "1"}, "employee": {"team_id": "1", "name": "1", "sname": "1", "exp": "1", "position": "1", "salary": "1", "coefficient": "1"}}
 
 
 if __name__ == '__main__':
